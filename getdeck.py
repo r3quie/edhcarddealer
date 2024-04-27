604988a243f6bcb5ce484a956049f805f243b462
r3quie <r3quie@gmail.com>
2024-04-27 14:58:13 +0200

    error message

--- getdeck.py
+++ getdeck.py
@@ -1,94 +1,94 @@
 #imported_deck = open("cache/deck.txt")
-imported_deck = """1 Aarakocra Sneak
-1 Access Tunnel
-1 Aether Tunnel
-1 Ancestral Vision
-1 Aqueous Form
-1 As Foretold
-1 Baleful Mastery
-1 Blackblade Reforged
-1 Blasphemous Act
-1 Bojuka Bog
-1 Braids, Conjurer Adept
-1 Cascade Bluffs
-1 Caves of Chaos Adventurer
-1 Chaos Warp
-1 Command Tower
-1 Counterspell
-1 Court of Ire
-1 Creeping Bloodsucker
-1 Crumbling Necropolis
-1 Darkwater Catacombs
-1 Defabricate
-1 Descent into Avernus
-1 Dimir Signet
-1 Dragonmaster Outcast
-1 Dragonskull Summit
-1 Drowned Catacomb
-1 Endless Evil
-1 Exotic Orchard
-1 Feed the Swarm
-1 Feywild Caretaker
-1 Gate to the Aether
-1 Indulgent Tormentor
-1 Infernal Grasp
-1 Inspiring Refrain
-6 Island
-1 Izzet Signet
-1 Kumena's Awakening
-1 Lizard Blades
-1 Mechanized Production
-1 Midnight Clock
-4 Mountain
-1 Negate
-1 Nightscape Familiar
-1 Palace Siege
-1 Passageway Seer
-1 Path of Ancestry
-1 Phyrexian Arena
-1 Plargg and Nassari
-1 Profane Tutor
-1 Protection Racket
-1 Rakdos Signet
-1 Ravenloft Adventurer
-1 Reliquary Tower
-1 Replicating Ring
-1 Rilsa Rael, Kingpin
-1 Ring of Evos Isle
-1 Ring of Valkas
-1 Ring of Xathrid
-1 Rogue's Passage
-1 Rousing Refrain
-1 Shivan Reef
-1 Skyline Despot
-1 Smoldering Marsh
-1 Sphinx of the Second Sun
-1 Star Whale
-1 Stirring Bard
-1 Stolen Strategy
-1 Sulfur Falls
-1 Sulfurous Springs
-1 Sunken Hollow
-2 Swamp
-1 Swiftfoot Boots
-1 Sword Coast Sailor
-1 Talisman of Creativity
-1 Talisman of Dominance
-1 Talisman of Indulgence
-1 Tavern Brawler
-1 Temple of Deceit
-1 Temple of Epiphany
-1 Temple of Malice
-1 Thassa, God of the Sea
-1 The Ninth Doctor
-1 The Tenth Doctor
-1 Thopter Spy Network
-1 Tomb of Horrors Adventurer
-1 Twilight Prophet
-1 Underground River
-1 Vandalblast
-1 Wheel of Fate
-1 Whispersilk Cloak
+imported_deck = """1 Aarakocra Sneak\r
+1 Access Tunnel\r
+1 Aether Tunnel\r
+1 Ancestral Vision\r
+1 Aqueous Form\r
+1 As Foretold\r
+1 Baleful Mastery\r
+1 Blackblade Reforged\r
+1 Blasphemous Act\r
+1 Bojuka Bog\r
+1 Braids, Conjurer Adept\r
+1 Cascade Bluffs\r
+1 Caves of Chaos Adventurer\r
+1 Chaos Warp\r
+1 Command Tower\r
+1 Counterspell\r
+1 Court of Ire\r
+1 Creeping Bloodsucker\r
+1 Crumbling Necropolis\r
+1 Darkwater Catacombs\r
+1 Defabricate\r
+1 Descent into Avernus\r
+1 Dimir Signet\r
+1 Dragonmaster Outcast\r
+1 Dragonskull Summit\r
+1 Drowned Catacomb\r
+1 Endless Evil\r
+1 Exotic Orchard\r
+1 Feed the Swarm\r
+1 Feywild Caretaker\r
+1 Gate to the Aether\r
+1 Indulgent Tormentor\r
+1 Infernal Grasp\r
+1 Inspiring Refrain\r
+6 Island\r
+1 Izzet Signet\r
+1 Kumena's Awakening\r
+1 Lizard Blades\r
+1 Mechanized Production\r
+1 Midnight Clock\r
+4 Mountainv
+1 Negate\r
+1 Nightscape Familiar\r
+1 Palace Siege\r
+1 Passageway Seer\r
+1 Path of Ancestry\r
+1 Phyrexian Arena\r
+1 Plargg and Nassari\r
+1 Profane Tutor\r
+1 Protection Racket\r
+1 Rakdos Signet\r
+1 Ravenloft Adventurer\r
+1 Reliquary Tower\r
+1 Replicating Ring\r
+1 Rilsa Rael, Kingpin\r
+1 Ring of Evos Isle\r
+1 Ring of Valkas\r
+1 Ring of Xathrid\r
+1 Rogue's Passage\r
+1 Rousing Refrain\r
+1 Shivan Reef\r
+1 Skyline Despot\r
+1 Smoldering Marsh\r
+1 Sphinx of the Second Sun\r
+1 Star Whale\r
+1 Stirring Bard\r
+1 Stolen Strategy\r
+1 Sulfur Falls\r
+1 Sulfurous Springs\r
+1 Sunken Hollow\r
+2 Swamp\r
+1 Swiftfoot Boots\r
+1 Sword Coast Sailor\r
+1 Talisman of Creativity\r
+1 Talisman of Dominance\r
+1 Talisman of Indulgence\r
+1 Tavern Brawler\r
+1 Temple of Deceit\r
+1 Temple of Epiphany\r
+1 Temple of Malice\r
+1 Thassa, God of the Sea\r
+1 The Ninth Doctor\r
+1 The Tenth Doctor\r
+1 Thopter Spy Network\r
+1 Tomb of Horrors Adventurer\r
+1 Twilight Prophet\r
+1 Underground River\r
+1 Vandalblast\r
+1 Wheel of Fate\r
+1 Whispersilk Cloak\r
 1 Obeka, Splitter of Seconds"""
 
 
